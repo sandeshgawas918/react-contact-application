8eb543eeddb4112ab0fd03c90c5358680211b50a
sandesh gawas <sandeshgawas918@gmail.com>
2023-12-30 19:13:14 +0530

    data will be cleared after submit

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
 import { IoIosPersonAdd } from "react-icons/io";
 
-const AddContact = ({Contact,setContact}) => {
-    let clearContact={
-        id:6,
-        name:'',
-        email:'',
-        mobile:'',
-        img:''
+const AddContact = ({ Contact, setContact }) => {
+    let clearContact = {
+        id: 6,
+        name: '',
+        email: '',
+        mobile: '',
+        img: ''
     }
 
     const [newContact, setnewContact] = useState(clearContact)
+    const [close, setclose] = useState(true)
 
-    let addNewContact=()=>{
-        let myNewContact={
-            id:6,
-            name:newContact.name,
-            email:newContact.email,
-            mobile:newContact.mobile,
-            img:newContact.img,
+    let addNewContact = () => {
+        let myNewContact = {
+            id: 6,
+            name: newContact.name,
+            email: newContact.email,
+            mobile: newContact.mobile,
+            img: newContact.img
         }
-        setContact([...Contact,myNewContact])
+        setContact([...Contact, myNewContact])
+        setnewContact(clearContact)
         console.log('new contact added');
     }
 
@@ -33,10 +35,10 @@ const AddContact = ({Contact,setContact}) => {
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content rounded-4">
-                        {/* <div class="modal-body"> */}
                         <div className="card bg-light text-black rounded-4 shadow-lg">
                             <div className="card-header p-3 text-center bg-success text-white rounded-top-4 fw-semibold">
                                 Add Contact Here
+                                <button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="card-body fw-semibold">
                                 <div className="row">
@@ -44,7 +46,7 @@ const AddContact = ({Contact,setContact}) => {
                                         <div className="col-form-label">Name :</div>
                                     </div>
                                     <div className="col-md-8">
-                                        <input value={newContact.name} onChange={(e)=>{setnewContact({...newContact,name:e.target.value})}} type="text" className="form-control" />
+                                        <input value={newContact.name} onChange={(e) => { setnewContact({ ...newContact, name: e.target.value }) }} type="text" className="form-control" />
                                     </div>
                                 </div>
                                 <div className="row mt-2">
@@ -52,7 +54,7 @@ const AddContact = ({Contact,setContact}) => {
                                         <div className="col-form-label">Email : </div>
                                     </div>
                                     <div className="col-md-8">
-                                        <input value={newContact.email} onChange={(e)=>{setnewContact({...newContact,email:e.target.value})}} type="text" className="form-control"
+                                        <input value={newContact.email} onChange={(e) => { setnewContact({ ...newContact, email: e.target.value }) }} type="text" className="form-control"
                                         />
                                     </div>
                                 </div>
@@ -61,7 +63,7 @@ const AddContact = ({Contact,setContact}) => {
                                         <div className="col-form-label">Mobile : </div>
                                     </div>
                                     <div className="col-md-8">
-                                        <input value={newContact.mobile} onChange={(e)=>{setnewContact({...newContact,mobile:e.target.value})}} type="text" className="form-control"
+                                        <input value={newContact.mobile} onChange={(e) => { setnewContact({ ...newContact, mobile: e.target.value }) }} type="text" className="form-control"
                                         />
                                     </div>
                                 </div>
@@ -70,7 +72,7 @@ const AddContact = ({Contact,setContact}) => {
                                         <div className="col-form-label">Img : </div>
                                     </div>
                                     <div className="col-md-8">
-                                        <input value={newContact.img} onChange={(e)=>{setnewContact({...newContact,img:e.target.value})}} type="text" className="form-control"
+                                        <input value={newContact.img} onChange={(e) => { setnewContact({ ...newContact, img: e.target.value }) }} type="text" className="form-control"
                                         />
                                     </div>
                                 </div>
@@ -78,11 +80,11 @@ const AddContact = ({Contact,setContact}) => {
                                     <button onClick={addNewContact} className="btn btn-success text-white rounded-3">Submit</button>
                                 </div>
                             </div>
-                            {/* </div> */}
                         </div>
                     </div>
                 </div>
             </div>
+            
         </div>
     )
 }
