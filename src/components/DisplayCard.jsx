0298a8ab332332a4ba9a55e9d041dc25c58d0f62
sandesh gawas <sandeshgawas918@gmail.com>
2023-12-31 19:39:50 +0530

    router added with new route

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { MdDelete } from "react-icons/md";
 import { FaEye } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const DisplayCard = ({ search, Contact, setContact,sort,order }) => {
 
@@ -42,8 +43,8 @@ const DisplayCard = ({ search, Contact, setContact,sort,order }) => {
                                             </ul>
                                         </div>
                                         <div className="col-md-1 d-flex flex-row flex-sm-column justify-content-evenly align-items-center">
-                                            <button className="btn btn-warning"><FaEye /></button>
-                                            <button className="btn btn-primary"><FaEdit /></button>
+                                            <Link to={'/viewContact'} className="btn btn-warning"><FaEye /></Link>
+                                            <Link to={'/editContact'} className="btn btn-primary"><FaEdit /></Link>
                                             <button className="btn btn-danger" onClick={() => { deleteContact(e.id) }} ><MdDelete /></button>
                                         </div>
                                     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import ViewContact from './components/ViewContact.jsx'
+import EditContact from './components/EditContact.jsx'
+import Navbar from './components/Navbar.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/react-contact-application",
+    element: <App />
+  },
+  {
+    path: "/viewContact",
+    element: <ViewContact />
+  },
+  {
+    path: "/editContact",
+    element: <EditContact />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Navbar/>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
